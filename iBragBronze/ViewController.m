d19d51ea837c8d6be3715e79a88e07eef279f13b
Andrey Balakin <balakin@stexgroup.com>
2013-09-13 17:55:46 +0400

    changed facebook id, added app icons and itunes urls

--- iBragBronze/ViewController.m
+++ iBragBronze/ViewController.m
@@ -25,6 +25,8 @@
 	IBOutlet UIButton *buttonTwo;
 	IBOutlet UIButton *buttonThree;
 	IBOutlet UIButton *buttonFour;
+	NSString * urlString;
+	NSString * appID;
 }
 
 @end
@@ -41,24 +43,34 @@
 	labelVersionType.textColor = blackColor;
 	[buttonOne setTitle:@"Bronze" forState:UIControlStateNormal];
 	[buttonOne setTitleColor:bronzeColor forState:UIControlStateNormal];
+	urlString = @"https://itunes.apple.com/us/app/bragging-rights-black/id705832378?ls=1&mt=8"; //black
+	appID = @"705832378";
 #elif BRONZE
 	labelVersionType.text = @"Bronze";
 	labelVersionType.textColor = bronzeColor;
+	urlString = @"https://itunes.apple.com/us/app/bragging-rights-bronze/id705824522?ls=1&mt=8"; //bronze
+	appID = @"705824522";
 #elif SILVER
 	labelVersionType.text = @"Silver";
 	labelVersionType.textColor = silverColor;
 	[buttonFour setTitle:@"Bronze" forState:UIControlStateNormal];
 	[buttonFour setTitleColor:bronzeColor forState:UIControlStateNormal];
+	urlString = @"https://itunes.apple.com/us/app/bragging-rights-silver/id705832421?ls=1&mt=8"; //silver
+	appID = @"705832421";
 #elif PLATINUM
 	labelVersionType.text = @"Platinum";
 	labelVersionType.textColor = platinumColor;
 	[buttonTwo setTitle:@"Bronze" forState:UIControlStateNormal];
 	[buttonTwo setTitleColor:bronzeColor forState:UIControlStateNormal];
+	urlString = @"https://itunes.apple.com/us/app/bragging-rights-platinum/id705832495?ls=1&mt=8"; //platinum
+	appID = @"705832495";
 #elif GOLD
 	labelVersionType.text = @"Gold";
 	labelVersionType.textColor = goldColor;
 	[buttonThree setTitle:@"Bronze" forState:UIControlStateNormal];
 	[buttonThree setTitleColor:bronzeColor forState:UIControlStateNormal];
+	urlString = @"https://itunes.apple.com/us/app/bragging-rights-gold/id705832538?ls=1&mt=8"; //gold
+	appID = @"705832538";
 #endif
 }
 
@@ -73,17 +85,22 @@
 - (IBAction)buttonEmailPressed {
     [Flurry logEvent:[NSString stringWithFormat:@"Email button pressed in %@ version", labelVersionType.text]];
 	if ([MFMailComposeViewController canSendMail]){
-		NSString *message = [NSString stringWithFormat:@"Get %@ Bragging Rights for yourself too!", labelVersionType.text];
+		NSString *message = [NSString stringWithFormat:@"Get <a href='%@'>Bragging Rights %@</a> for yourself too!<br /><br />", urlString, labelVersionType.text];
 		
 		MFMailComposeViewController *picker = [[MFMailComposeViewController alloc] init];
 		
 		
 		picker.mailComposeDelegate = self;
 		[picker setToRecipients:[NSArray array]];
-		[picker setSubject:[NSString stringWithFormat:@"I just bought %@ Bragging Rights", labelVersionType.text]];
-		[picker setMessageBody:message isHTML:NO];
+		[picker setSubject:[NSString stringWithFormat:@"I just bough Bragging Rights %@", labelVersionType.text]];
+		[picker setMessageBody:message isHTML:YES];
 		picker.modalPresentationStyle = UIModalPresentationFormSheet;
 		picker.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+		
+		// Attach an image to the email
+		NSString * pathToFile = [[NSBundle mainBundle] pathForResource:@"icon144" ofType:@"png"];
+		NSData *myData = [NSData dataWithContentsOfFile:pathToFile];
+		[picker addAttachmentData:myData mimeType:@"image/png" fileName:@"icon"];
 		
 		[self presentModalViewController:picker animated:YES];
 	}
@@ -95,7 +112,8 @@
 
 - (IBAction)buttonBuyPressed {
     [Flurry logEvent:[NSString stringWithFormat:@"Share button pressed in %@ version", labelVersionType.text]];
-	NSURL * url = [NSURL URLWithString:@"https://buy.itunes.apple.com/WebObjects/MZFinance.woa/wa/giftSongsWizard"];
+	
+	NSURL * url = [NSURL URLWithString:[NSString stringWithFormat:@"https://buy.itunes.apple.com/WebObjects/MZFinance.woa/wa/giftSongsWizard?gift=1&salableAdamId=%@&productType=C&pricingParameter=STDQ", appID]];
 	
 	[[UIApplication sharedApplication] openURL:url];
 }
@@ -130,8 +148,10 @@
 
 - (IBAction)buttonBuyNewTypePressed:(UIButton *)sender {
 	NSLog(@"%@ button pressed in %@ version", [sender titleForState:UIControlStateNormal], labelVersionType.text);
+	
 	[Flurry logEvent:[NSString stringWithFormat:@"%@ button pressed in %@ version", [sender titleForState:UIControlStateNormal], labelVersionType.text]];
-	NSURL * url = [NSURL URLWithString:[NSString stringWithFormat:@"https://itunes.apple.com/%@", [[sender titleForState:UIControlStateNormal] lowercaseString]]];
+	
+	NSURL * url = [NSURL URLWithString:urlString];
 	
 	[[UIApplication sharedApplication] openURL:url];
 }
@@ -140,11 +160,10 @@
 
 - (void)postToFacebook
 {
-    NSDictionary* params = @{@"name": [NSString stringWithFormat:@"I just bought %@ Bragging Rights", labelVersionType.text],
+    NSDictionary* params = @{@"name": [NSString stringWithFormat:@"I just bought Bragging Rights %@", labelVersionType.text],
                              @"caption": @"get one for yourself too",
                              //@"description": @"get one for yourself too",
-                             @"link": [NSString stringWithFormat:@"https://itunes.apple.com/bogus-link/%@", [labelVersionType.text lowercaseString]]
-                             };
+                             @"link": urlString};
     
     [FBWebDialogs presentFeedDialogModallyWithSession:nil
                                            parameters:params
